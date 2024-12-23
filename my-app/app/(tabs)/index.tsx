535c1317ed6f6eb53399c8ff22bbcc14ab440ad7
nikola.sovilj <nikola.sovilj@crqlar.com>
2024-12-23 01:06:51 +0100

    added marker

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,27 +1,28 @@
 import React from "react";
-import { Platform, StyleSheet, View, Text } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
-    if (Platform.OS === "web") {
-        return (
-            <View style={styles.container}>
-                <Text>Maps are not supported on the web!</Text>
-            </View>
-        );
-    }
-
     return (
         <View style={styles.container}>
             <MapView
-                style={styles.map}
+                style={StyleSheet.absoluteFill}
                 initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
+                    latitude: 45.250445, // Latitude for Adija Endrea 33, Novi Sad
+                    longitude: 19.830203, // Longitude for Adija Endrea 33, Novi Sad
+                    latitudeDelta: 0.01, // Zoom level for latitude
+                    longitudeDelta: 0.01, // Zoom level for longitude
                 }}
-            />
+            >
+                <Marker
+                    coordinate={{
+                        latitude: 45.250445,
+                        longitude: 19.830203,
+                    }}
+                    title="Adija Endrea 33"
+                    description="Novi Sad, Serbia"
+                />
+            </MapView>
         </View>
     );
 }
@@ -29,11 +30,5 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    map: {
-        flex: 1,
-        width: "100%",
     },
 });
