2c2e015fcb3a50e7549c3ed4e876dce3641ad395
nikola.sovilj <nikola.sovilj@crqlar.com>
2024-12-23 22:58:40 +0100

    change

--- my-app/service/api.ts
+++ my-app/service/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import Constants from "expo-constants/src/Constants";
 
 export const SERVER_PORT = 8080
-const localIP = Constants.manifest2.extra.expoGo.debuggerHost.split(':')[0]
-const API_URL = `http://${localIP}:${SERVER_PORT}`
+export const LOCAL_IP = Constants.manifest2.extra.expoGo.debuggerHost.split(':')[0]
+export const API_URL = `http://${LOCAL_IP}:${SERVER_PORT}`
 
 const api = axios.create({
     baseURL: API_URL
