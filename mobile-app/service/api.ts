c8891fab5822e7d88fab43b3b6422c62fd96d5dc
mihalsabados <sabadosmihal00@gmail.com>
2025-01-28 23:22:54 +0100

    upload to eas

--- mobile-app/service/api.ts
+++ mobile-app/service/api.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 import Constants from "expo-constants/src/Constants";
 
-export const SERVER_PORT = 8080
-export const LOCAL_IP = Constants.manifest2.extra.expoGo.debuggerHost.split(':')[0]
-export const API_URL = `http://${LOCAL_IP}:${SERVER_PORT}`
+export const SERVER_PORT = 8080;
+export const LOCAL_IP =
+  Constants.manifest2.extra.expoGo.debuggerHost.split(":")[0];
+// export const API_URL = `http://${LOCAL_IP}:${SERVER_PORT}`
+export const API_URL = `https://foxspot.onrender.com`;
 
 const api = axios.create({
-    baseURL: API_URL
+  baseURL: API_URL,
 });
 
 export default api;
