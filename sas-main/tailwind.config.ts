3bbe5b95cd53e14ff9309e630b683aa3314a8ffc
hammerns <62013861+hammerns@users.noreply.github.com>
2025-02-19 21:53:30 +0100

    Add files via upload

--- sas-main/tailwind.config.ts
+++ sas-main/tailwind.config.ts
@@ -33,8 +33,8 @@ module.exports = {
         "bg-img-1": "url('/img-1.png')",
         "bg-img-2": "url('/img-2.png')",
         "feature-bg": "url('/')",
-        pattern: "url('/iphone.png')",
-        "pattern-2": "url('/pattern-bg.png')",
+        pattern: "url('/pattern.png')",
+        "pattern-2": "url('/pattern-bg2.png')",
       },
       screens: {
         xs: "400px",
