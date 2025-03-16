219211e9c00c361b2c501c8457a57e6851756f1d
nikola.sovilj <nikola.sovilj@crqlar.com>
2025-03-16 17:39:58 +0100

    Commented LOCAL_IP (extra is not defined)

--- mobile-app/service/api.ts
+++ mobile-app/service/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import Constants from "expo-constants/src/Constants";
 
 export const SERVER_PORT = 8080;
-export const LOCAL_IP =
-  Constants.manifest2.extra.expoGo.debuggerHost.split(":")[0];
+// export const LOCAL_IP =
+//   Constants.manifest2.extra.expoGo.debuggerHost.split(":")[0];
 // export const API_URL = `http://${LOCAL_IP}:${SERVER_PORT}`
 export const API_URL = `https://foxspot.onrender.com`;
 
